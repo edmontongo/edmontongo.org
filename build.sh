5175bef087483b5f2eb129b12e5bc61dc4ec671e
Nathan Youngman <git@nathany.com>
2014-12-23 12:14:32 -0700

    build css file with sassc
    not adding this to Travis for now.

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-sass --update sass/all.sass:static/assets/all.css --load-path sass/ --force --style compressed
-# sassc -o static/assets/all.css -I sass/ -t compressed sass/all.sass
-hugo
+sassc sass/all.sass static/assets/all.css --style compressed
