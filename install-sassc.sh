51c631f35326593038a44b2278cabbd65c711e12
Nathan Youngman <git@nathany.com>
2014-12-25 10:38:43 -0700

    sassc conditional build

--- install-sassc.sh
+++ install-sassc.sh
@@ -2,18 +2,16 @@ set -x
 set -e
 
 # sassc (3.0.2)
+if [ ! -e $CIRCLE_BUILD_DIR/bin/sassc ]; then
+  export SASS_BUILD_DIR=$HOME/src/github.com/sass
+  mkdir -p $CIRCLE_BUILD_DIR/bin $SASS_BUILD_DIR
 
-export SASS_BUILD_DIR=$HOME/src/github.com/sass
+  cd $SASS_BUILD_DIR
+  git clone --recursive https://github.com/sass/libsass.git -b 3.0.2
+  git clone https://github.com/sass/sassc.git -b 3.0.2
 
-mkdir -p $CIRCLE_BUILD_DIR/bin $HOME/src/github.com/sass
+  cd sassc
+  SASS_LIBSASS_PATH=$SASS_BUILD_DIR/libsass make
 
-cd $HOME/src/github.com/sass
-git clone --recursive https://github.com/sass/libsass.git -b 3.0.2
-git clone https://github.com/sass/sassc.git -b 3.0.2
-
-cd sassc
-SASS_LIBSASS_PATH=$HOME/src/github.com/sass/libsass make
-
-cp $HOME/src/github.com/sass/sassc/bin/sassc $CIRCLE_BUILD_DIR/bin/sassc
-
-echo $SASS_BUILD_DIR
+  cp $SASS_BUILD_DIR/sassc/bin/sassc $CIRCLE_BUILD_DIR/bin/sassc
+fi
