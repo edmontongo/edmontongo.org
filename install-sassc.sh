db58a7d706df185b1994339ddb8c8332c79837cc
Nathan Youngman <git@nathany.com>
2014-12-25 10:36:10 -0700

    CIRCLE_BUILD_DIR

--- install-sassc.sh
+++ install-sassc.sh
@@ -3,14 +3,17 @@ set -e
 
 # sassc (3.0.2)
 
-pwd
-mkdir -p ./bin $HOME/src/github.com/sass
-cd $HOME/src/github.com/sass
+export SASS_BUILD_DIR=$HOME/src/github.com/sass
 
+mkdir -p $CIRCLE_BUILD_DIR/bin $HOME/src/github.com/sass
+
+cd $HOME/src/github.com/sass
 git clone --recursive https://github.com/sass/libsass.git -b 3.0.2
 git clone https://github.com/sass/sassc.git -b 3.0.2
 
 cd sassc
 SASS_LIBSASS_PATH=$HOME/src/github.com/sass/libsass make
 
-# cp $HOME/src/github.com/sass/sassc/bin/sassc ./bin/sassc
+cp $HOME/src/github.com/sass/sassc/bin/sassc $CIRCLE_BUILD_DIR/bin/sassc
+
+echo $SASS_BUILD_DIR
