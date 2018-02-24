b7b2cc354242eecb3abb783cd74655936fd42c75
Nathan Youngman <git@nathany.com>
2018-02-23 18:38:32 -0700

    watch: build files published in the future
    (newer version of Hugo locally)

--- watch.sh
+++ watch.sh
@@ -4,4 +4,4 @@ SASSC_COMMAND='sassc sass/all.sass static/assets/all.css --style compressed'
 eval $SASSC_COMMAND
 # watch Sass and Hugo files during development
 notify -c ${SASSC_COMMAND} sass/** &
-hugo server -v -D --watch && fg
+hugo server -v -D -F --watch && fg
