d23cd6f47baf389f3fe209cf62725176fbf3e057
Nathan Youngman <git@nathany.com>
2018-02-27 19:11:23 -0700

    upgrade Hugo deployment
    based on https://github.com/nathany/hugo-deploy

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,11 +1,12 @@
-HUGO_VERSION=0.16
-HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_linux-64bit.tgz
+HUGO_VERSION=0.37
+HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 
 set -x
 set -e
 
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
-  tar xvzf ${HUGO_DOWNLOAD} -C $CIRCLE_BUILD_DIR/bin/
+  wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
+  tar xvzf ${HUGO_DOWNLOAD} hugo
+  mv  hugo $CIRCLE_BUILD_DIR/bin/hugo
 fi
--- watch.sh
+++ watch.sh
@@ -2,6 +2,15 @@ SASSC_COMMAND='sassc sass/all.sass static/assets/all.css --style compressed'
 
 # execute SassC at startup
 eval $SASSC_COMMAND
+
+# Ctrl-C kills both commands
+trap killgroup SIGINT
+killgroup(){
+  kill 0
+}
+
 # watch Sass and Hugo files during development
-notify -c ${SASSC_COMMAND} sass/** &
+notify -c "${SASSC_COMMAND}" sass/** &
 hugo server -v -D -F --watch && fg
+
+
