508ac7bbbb0344643831b18ba6db151ed2f8c605
Nathan Youngman <git@nathany.com>
2018-07-15 10:07:51 -0600

    Hugo 0.44 extended: asset pipeline

--- watch.sh
+++ watch.sh
@@ -1,16 +1,3 @@
-SASSC_COMMAND='sassc sass/all.scss static/assets/all.css --style compressed'
-
-# execute SassC at startup
-eval $SASSC_COMMAND
-
-# Ctrl-C kills both commands
-trap killgroup SIGINT
-killgroup(){
-  kill 0
-}
-
-# watch Sass and Hugo files during development
-notify -c "${SASSC_COMMAND}" sass/** &
-hugo server -D -F --watch && fg
+hugo server -D -F --watch
 
 
